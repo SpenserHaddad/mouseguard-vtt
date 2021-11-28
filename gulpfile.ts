17ceac5516fe61197e2d680487bf01edaed6fb25
Spenser Haddad <spenser.haddad@gmail.com>
2021-11-27 22:00:16 -0500

    Refactor tasks, add watch option
    Move tasks into functions and make the high-level actions more reusable
    and consistent.
    
    This makes it easier for us to then add the watch script to
    automatically rebuild the project on a save.

--- gulpfile.ts
+++ gulpfile.ts
@@ -14,11 +14,19 @@ function handleError(err: Error) {
 }
 
 const SYSTEM_SCSS = "src/scss/**/*.scss";
+
+function compileTs() {
+    return gulp.src('src/**/*.ts')
+        .pipe(project())
+        .pipe(gulp.dest('dist/'))
+
+}
+
 function compileScss() {
-    let options = {
+    const options = {
         outputStyle: 'expanded'
     };
-    return gulp.src(SYSTEM_SCSS)
+    return gulp.src(SYSTEM_SCSS, { since: gulp.lastRun(compileScss) })
         .pipe(
             sass(options).on('error', handleError)
         )
@@ -28,41 +36,45 @@ function compileScss() {
         .pipe(gulp.dest('dist/css'));
 }
 
-gulp.task('css', compileScss);
-
-gulp.task('compile', () => {
-    return gulp.src('src/**/*.ts')
-        .pipe(project())
-        .pipe(gulp.dest('dist/'))
-})
-
-gulp.task('copy', async () => {
+function copyToDist() {
+    const sinceLastRun = { since: gulp.lastRun(copyToDist) }
     return new Promise<void>((resolve, reject) => {
-        gulp.src('README.md').pipe(gulp.dest("dist/"))
-        gulp.src("src/system.json").pipe(gulp.dest("dist/"))
-        gulp.src("src/template.json").pipe(gulp.dest("dist/"))
-        gulp.src("src/fonts/**").pipe(gulp.dest("dist/fonts/"))
-        gulp.src("src/lang/**").pipe(gulp.dest("dist/lang/"))
-        gulp.src("src/module/**").pipe(gulp.dest("dist/module/"))
-        gulp.src("src/packs/**").pipe(gulp.dest("dist/packs/"))
-        gulp.src("src/templates/**").pipe(gulp.dest("dist/templates/"))
+        gulp.src("README.md", sinceLastRun).pipe(gulp.dest("dist/"))
+        gulp.src("src/system.json", sinceLastRun).pipe(gulp.dest("dist/"))
+        gulp.src("src/template.json", sinceLastRun).pipe(gulp.dest("dist/"))
+        gulp.src("src/fonts/**", sinceLastRun).pipe(gulp.dest("dist/fonts/"))
+        gulp.src("src/lang/**", sinceLastRun).pipe(gulp.dest("dist/lang/"))
+        gulp.src("src/module/**", sinceLastRun).pipe(gulp.dest("dist/module/"))
+        gulp.src("src/packs/**", sinceLastRun).pipe(gulp.dest("dist/packs/"))
+        gulp.src("src/templates/**", sinceLastRun).pipe(gulp.dest("dist/templates/"))
         resolve();
     })
-})
+}
 
-gulp.task('build', gulp.parallel('compile', 'css', 'copy'));
-
-
-
-// This is supposed to copy the dist folder into the modules directory for testing. Only works if you've set it up the right way
-const MODULEPATH = process.env.FOUNDRY_DATA
-gulp.task('foundry', () => {
-    if (process.env.FOUNDRY_DATA) {
+// This is supposed to copy the dist folder into the modules directory for testing. 
+// Only works if you've set it up the right way
+function copyToFoundry() {
+    const MODULEPATH = process.env.FOUNDRY_DATA
+    if (MODULEPATH) {
         return gulp.src('dist/**').pipe(gulp.dest(`${MODULEPATH}/systems/mouseguard`))
     }
     else {
         console.log("FOUNDRY_DATA not set. Provide a path as either an envvar or in a .env file to automatically update a Foundry directory.")
         return gulp.src(".")
     }
-})
-gulp.task("update", gulp.series('build', 'foundry'))
+}
+
+gulp.task("css", compileScss)
+gulp.task("compile", compileTs)
+gulp.task("copy", copyToDist)
+gulp.task("foundry", copyToFoundry)
+const build = gulp.series("compile", "css", "copy")
+const update = gulp.series(build, "foundry")
+const watch = function () {
+    gulp.watch("src/", update)
+}
+console.log(watch)
+exports.build = build
+exports.update = update
+exports.watch = watch
+exports.default = update
