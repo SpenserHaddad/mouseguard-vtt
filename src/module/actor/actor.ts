ba0b2f42c521ed8f25f626de340242c4c91bf847
Spenser Haddad <spenser.haddad@gmail.com>
2022-02-21 17:22:49 -0500

    Add configName to props and init them for background

--- src/module/actor/actor.ts
+++ src/module/actor/actor.ts
@@ -1,3 +1,5 @@
+import { MouseGuardConfig } from "../helpers/config.js";
+
 export class MgActor extends Actor {
     background: MgActorBackground;
     abilities: MgActorAbilities;
@@ -13,29 +15,53 @@ export class MgActorAbilities {
 }
 
 export class MgActorBackground {
-    age: MgActorBackgroundAgeProp;
-    home: MgActorBackgroundProp;
-    furColor: MgActorBackgroundProp;
-    guardRank: MgActorBackgroundProp;
-    cloakColor: MgActorBackgroundProp;
-    parents: MgActorBackgroundProp;
-    artisan: MgActorBackgroundProp;
-    mentor: MgActorBackgroundProp;
-    friend: MgActorBackgroundProp;
-    enemy: MgActorBackgroundProp;
+    age: MgActorBackgroundAgeProp = new MgActorBackgroundAgeProp(MouseGuardConfig.background.age);
+    home: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.home);
+    furColor: MgActorProp = new MgActorStrProp(MouseGuardConfig.background.furColor);
+    guardRank: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.guardRank);
+    cloakColor: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.cloakColor);
+    parents: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.parents);
+    artisan: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.artisan);
+    mentor: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.mentor);
+    friend: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.friend);
+    enemy: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.enemy);
+
+    get allProperties(): MgActorProp[] {
+        return [
+            this.age,
+            this.home,
+            this.furColor,
+            this.guardRank,
+            this.cloakColor,
+            this.parents,
+            this.artisan,
+            this.mentor,
+            this.friend,
+            this.enemy];
+    }
 }
 
-export class MgActorBackgroundProp<T = string> {
+export interface MgActorProp {
+    label: string;
+    readonly configName: string;
+}
+
+export class MgActorStrProp<T = string> implements MgActorProp {
     label: string;
     value: T;
+    readonly configName: string
+
+    constructor(configName: string) {
+        this.configName = configName;
+    }
 }
 
-export class MgActorBackgroundAgeProp extends MgActorBackgroundProp<number> {
+export class MgActorBackgroundAgeProp extends MgActorStrProp<number> {
     readonly min = 0;
     readonly max = 50;
 
-    constructor(age: number = 18) {
-        super();
+    constructor(configName: string, age: number = 18) {
+        super(configName);
         this.value = age;
     }
 }
