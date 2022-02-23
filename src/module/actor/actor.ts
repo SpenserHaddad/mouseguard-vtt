02f8d067b4639dbd0c99c2d90ac89a71b8f9ba12
Spenser Haddad <spenser.haddad@gmail.com>
2022-02-22 19:51:51 -0500

    Proper min and max values and advancement rules for skills and abilities
    Correctly implement the following rules for skills and abilities:
    * All skills have a min value of 1 and a max of 6.
    * Abilities have different mins and maxes as described in the rulebook.
    * The number of required passes and fails is based off the current
    stat's value.
        * You need <value> passes and <value -1> failures to advance.
    
    All of the above is properly coded in the actor class now.

--- src/module/actor/actor.ts
+++ src/module/actor/actor.ts
@@ -6,13 +6,48 @@ export class MgActor extends Actor {
     abilities: MgActorAbilities;
     skills: MgActorSkills;
 }
-
+// TODO: Max 24 skills
 export class MgActorSkills {
+    fighter = new MgActorSkill(MouseGuardConfig.skills.fighter);
+    healer = new MgActorSkill(MouseGuardConfig.skills.healer);
+    hunter = new MgActorSkill(MouseGuardConfig.skills.hunter);
+    instructor = new MgActorSkill(MouseGuardConfig.skills.instructor);
+    pathfinder = new MgActorSkill(MouseGuardConfig.skills.pathfinder);
+    scout = new MgActorSkill(MouseGuardConfig.skills.scout);
+    survivalist = new MgActorSkill(MouseGuardConfig.skills.survivalist);
+    weatherWatcher = new MgActorSkill(MouseGuardConfig.skills.weatherWatcher);
+    manipulator = new MgActorSkill(MouseGuardConfig.skills.manipulator);
+    orator = new MgActorSkill(MouseGuardConfig.skills.orator);
+    persuader = new MgActorSkill(MouseGuardConfig.skills.persuader);
 
+    get allProperties(): IMgActorProp[] {
+        return [
+            this.fighter,
+            this.healer,
+            this.hunter,
+            this.instructor,
+            this.pathfinder,
+            this.scout,
+            this.survivalist,
+            this.weatherWatcher,
+            this.manipulator,
+            this.orator,
+            this.persuader,
+        ]
+    }
 }
 
 export class MgActorAbilities {
+    nature = new MgActorAbility(MouseGuardConfig.abilities.nature, 0, 7);
+    will = new MgActorAbility(MouseGuardConfig.abilities.will, 1, 6);
+    health = new MgActorAbility(MouseGuardConfig.abilities.health, 1, 6);
+    resources = new MgActorAbility(MouseGuardConfig.abilities.resources, 1, 10);
+    circles = new MgActorAbility(MouseGuardConfig.abilities.circles, 1, 10);
 
+
+    get allProperties(): IMgActorProp[] {
+        return [this.nature, this.will, this.health, this.resources, this.circles];
+    }
 }
 
 export class MgActorCharacteristics {
@@ -20,7 +55,7 @@ export class MgActorCharacteristics {
     goal: MgActorStrProp;
     instinct: MgActorStrProp;
 
-    get allProperties(): MgActorProp[] {
+    get allProperties(): IMgActorProp[] {
         return [this.belief, this.goal, this.instinct]
     }
 }
@@ -28,7 +63,7 @@ export class MgActorCharacteristics {
 export class MgActorBackground {
     age: MgActorBackgroundAgeProp = new MgActorBackgroundAgeProp(MouseGuardConfig.background.age);
     home: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.home);
-    furColor: MgActorProp = new MgActorStrProp(MouseGuardConfig.background.furColor);
+    furColor: IMgActorProp = new MgActorStrProp(MouseGuardConfig.background.furColor);
     guardRank: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.guardRank);
     cloakColor: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.cloakColor);
     parents: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.parents);
@@ -37,7 +72,7 @@ export class MgActorBackground {
     friend: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.friend);
     enemy: MgActorStrProp = new MgActorStrProp(MouseGuardConfig.background.enemy);
 
-    get allProperties(): MgActorProp[] {
+    get allProperties(): IMgActorProp[] {
         return [
             this.age,
             this.home,
@@ -52,12 +87,98 @@ export class MgActorBackground {
     }
 }
 
-export interface MgActorProp {
+export interface IMgActorProp {
     label: string;
     readonly configName: string;
+
 }
 
-export class MgActorStrProp<T = string> implements MgActorProp {
+
+export class MgActorAbility implements IMgActorProp {
+    label: string;
+    configName: string;
+    _value: number;
+    min_value: number;
+    max_value: number;
+    _passes: number = 0;
+    _failures: number = 0;
+
+    constructor(configName: string, min_value: number, max_value: number) {
+        this.configName = configName;
+        this.min_value = min_value;
+        this.max_value = max_value;
+        this.value = min_value;
+    }
+
+    get value(): number {
+        return this._value;
+    }
+
+    set value(new_value: number) {
+        if (new_value < this.min_value || new_value > this.max_value) {
+            throw new RangeError(`Value must be between ${this.min_value} and ${this.max_value}`);
+        }
+        this._value = new_value;
+    }
+
+    get passes(): number {
+        return this._passes;
+    }
+
+    set passes(value: number) {
+        if (value < 0 || value > this.passesNeeded) {
+            throw new RangeError(`Number of passes must be between 0 and ${this.passesNeeded}`);
+        }
+        this._passes = value;
+    }
+
+    get failures(): number {
+        return this._failures;
+    }
+
+    set failures(value: number) {
+        if (value < 0 || value > this.failuresNeeded) {
+            throw new RangeError(`Number of failures must be between 0 and ${this.failuresNeeded}`);
+        }
+        this._passes = value;
+    }
+
+    get passesNeeded(): number {
+        return this.value;
+    }
+
+    get failuresNeeded(): number {
+        return this.value - 1;
+    }
+
+    get canAdvance(): boolean {
+        if (this.value == this.max_value) {
+            return false;
+        }
+        return (this.passes == this.passesNeeded) && (this.failures == this.failuresNeeded);
+    }
+
+    advanceSkill(force: boolean = false) {
+        if (!this.canAdvance && !force) {
+            throw new Error("Stat does not meet criteria for advancement.") // TODO: proper exception class
+        }
+        else if (this.value == this.max_value) {
+            throw new Error("Stat is already at max value")
+        }
+        this.value += 1;
+        this.passes = 0;
+        this.failures = 0;
+    }
+}
+
+export class MgActorSkill extends MgActorAbility {
+    constructor(configName: string) {
+        super(configName, 1, 6);
+    }
+
+}
+
+export class MgActorStrProp<T = string> implements IMgActorProp {
     label: string;
     value: T;
     readonly configName: string
